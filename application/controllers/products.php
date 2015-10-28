871ea848bfea783f6a7af561682fcd4ca8dbb0ae
HyperbolicSine <sinh9x0@gmail.com>
2015-10-28 16:21:43 -0700

    Updated first/last links on pagination

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -36,6 +36,10 @@ class products extends CI_Controller {
         $config['cur_tag_open'] = '<li class="active"><a href="#">';
         $config['cur_tag_close'] = '</a></li>';
         $config['prev_link'] = '&laquo;';
+        $config['last_tag_open'] ='<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['first_tag_open'] ='<li>';
+        $config['first_tag_close'] = '</li>';
         $config['prev_tag_open'] ='<li>';
         $config['prev_tag_close'] = '</li>';
         $config['next_link'] = '&raquo;';
