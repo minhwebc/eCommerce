676077c9ffe9a1b5ce44cd62faa4adb21e72e24e
HyperbolicSine <sinh9x0@gmail.com>
2015-10-28 23:39:04 -0700

    Improved orders code

--- application/views/orders.php
+++ application/views/orders.php
@@ -74,19 +74,12 @@
                         <td><form action='/orders/update_status' method='post'>
                             <input type="hidden" name="order_id" value="<?= $order->id ?>">
                             <select class="<?= $order->status ?>" name="status">
-                                <?php if($order->status == 'Order in process'){?>
-                                    <option value="process"><?= $order->status ?></option>
-                                    <option value="Shipped">Shipped</option>
-                                    <option value="Cancelled">Cancelled</option>
-                                <?php }else if($order->status == 'Shipped'){ ?>
-                                    <option value="Shipped"><?= $order->status ?></option>
-                                    <option value="Cancelled">Cancelled</option>
-                                    <option value="process">Order in process</option>
-                                <?php }else{ ?>
-                                    <option value="Cancelled"><?= $order->status ?></option>
-                                    <option value="Shipped">Shipped</option>
-                                    <option value="process">Order in process</option>
-                                <?php } ?>
+                                <option <?php if($order->status == 'Order in process'){
+                                        echo "selected = 'selected'"; } ?> value="process">Processing</option>
+                                <option <?php if($order->status == 'Shipped'){
+                                        echo "selected = 'selected'"; } ?> value="shipped">Shipped</option>
+                                <option <?php if($order->status == 'Cancelled'){
+                                        echo "selected = 'selected'"; } ?> value="cancelled">Cancelled</option
                             </select>
                             <input type='hidden' name='status' value=<?= $status?>>
                         </form></td>
