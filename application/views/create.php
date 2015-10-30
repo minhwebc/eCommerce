010b203bf72739cb14bb7f59d6137ad36dce7364
HyperbolicSine <sinh9x0@gmail.com>
2015-10-29 22:33:37 -0700

    Made create and edit pages consistent

--- application/views/create.php
+++ application/views/create.php
@@ -5,11 +5,9 @@
         <title>Create Product</title>
         <?php $this->load->view('partials/meta'); ?>
         <style type="text/css">
-
-            .jumbotron, .navbar {
-                background-color: white;
-                border: 1px solid e7e7e7;
-            }
+            .form-horizontal input {
+                width: 350px;
+            }   
 
             .form-control {
                 width: 350px;
@@ -20,71 +18,6 @@
     <body>
         <div class="container">
             <?php $this->load->view('partials/nav-admin'); ?>
-<<<<<<< HEAD
-
-	<h2>Add a product</h2>	
-	
-  <form class="from-inline form-horizontal" action="/dashboard/create_product" enctype="multipart/form-data" method="post">
-		<input type="hidden" name="id" value="">
-  		<div class="form-group">
-    		<label for="name" class="col-sm-2 control-label">Name: </label>
-    		<div class="col-sm-5">
-      			<input type="text" class="form-control" name='name'  id="prod_name">
-    		</div>
-  		</div>
-
-  		<div class="form-group">
-    		<label for="name" class="col-sm-2 control-label">Price </label>
-    		<div class="col-sm-5">
-      			<input type="text" class="form-control"  name='price' id="prod_name" >
-    		</div>
-  		</div>
-
- 		<div class="form-group">
-    		<label for="description" class="col-sm-2 control-label">Description: </label>
-    		<div class="col-sm-5">
-      			<textarea class="form-control"  rows ="5" name='description' id="prod_text"></textarea>
-   			</div>
-  		</div>
-
-      <div class="form-group">
-        <label for="name" class="col-sm-2 control-label">Inventory Count </label>
-        <div class="col-sm-5">
-            <input type="text" class="form-control"  name='inventory_count' id="prod_name">
-        </div>
-      </div>
-			
-    <div class="form-group">
-        <label for="name" class="col-sm-2 control-label">or add a new category: </label>
-        <div class="col-sm-5">
-            <input type="text" name ="category_new" class="form-control" id="prod_name" placeholder="New category....">
-        </div>
-      </div>
-
-      <div class="form-group">
-			  <div class="col-sm-5 col-sm-offset-2">
-        <label for="sell">Select list:</label>
-			  <select name ="category" class="form-control" id="sell">
-          <?php foreach($cat_results as $cat_result){?>
-			    <option value = "<?= $cat_result['id']?>"><?= $cat_result['name']?></option>
-			   <?php }?>
-        </select>
-			</div>
-
-  		<div class="form-group">
-        <div class="col-sm-5 col-sm-offset-2">
-    		<br><label for="exampleInputFile">Images</label>
-   		 	<input type="file" name ="userfile" size ="20" id="exampleInputFile">
-        <br>
-			
-			<input type="submit" value="Add a new product">
-		</div>
-		</form>
-
-		</div>
-	</body>
-	</html>
-=======
             <h2>Add a product</h2>	
             <form class="form-horizontal" action="/dashboard/create_product" enctype="multipart/form-data" method="post">
                 <input type="hidden" name="id" value="">
@@ -97,17 +30,17 @@
                 <div class="form-group">
                     <label for="name" class="col-sm-2 control-label">Price </label>
                     <div class="col-sm-6">
-                        <input type="text" class="form-control"  name='price' id="prod_name" >
+                        <input type="text" class="form-control"  name='price'>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="description" class="col-sm-2 control-label">Description: </label>
                     <div class="col-sm-6">
-                        <textarea class="form-control"  rows ="5" name='description' id="prod_text"></textarea>
+                        <textarea class="form-control" rows ="5" name='description' id="prod_text"></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                      <label for="sell">Select list:</label>
+                    <label for="sell" class="col-sm-2 control-label" >Select list:</label>
                     <div class="col-sm-6">
                         <select name ="category" class="form-control" id="sell">
                             <?php foreach($cat_results as $cat_result){?>
@@ -115,24 +48,31 @@
                             <?php }?>
                         </select>
                     </div>
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">or add a new category: </label>
-                        <input type="text" name ="category_new" class="form-control" id="prod_name" placeholder="New category...">
+                </div>
+                <div class="form-group">
+                    <label for="name" class="col-sm-2 control-label">Or add a new category: </label>
+                    <div class="col-sm-6">  
+                        <input type="text" name ="category_new" class="form-control">
                     </div>
                 </div>
                 <div class="form-group">
-                <label for="name" class="col-sm-2 control-label">Inventory Count </label>
-                <div class="col-sm-6">
-                    <input type="text" class="form-control"  name='inventory_count' id="prod_name">
-                </div>
+                    <label for="name" class="col-sm-2 control-label">Inventory Count: </label>
+                    <div class="col-sm-6">
+                        <input type="text" class="form-control"  name='inventory_count'>
+                    </div>
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputFile">Images</label>
-                    <input type="file" name ="userfile" size ="20" id="exampleInputFile"><br>
-                    <input type="submit" value="Add a new product">
+                    <label for="exampleInputFile" class="col-sm-2 control-label">Image: </label>
+                    <div class="col-sm-6">
+                        <input type="file" name ="userfile" size ="20" id="exampleInputFile">
+                    </div>
+                </div>
+                <div class="form-group" >
+                    <div class="col-sm-offset-2 col-sm-2">
+                        <input type="submit" value="Add a new product">
+                    </div>
                 </div>
             </form>
         </div>
     </body>
 </html>
->>>>>>> FETCH_HEAD
--- application/views/edit.php
+++ application/views/edit.php
@@ -1,42 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-	<meta charset="UTF-8">
-	<title>Edit Product</title>
-    <?php $this->load->view('partials/meta'); ?>
-    <script>
-		$(document.body).on( 'click', '.dropdown-menu li', function( event ) {
-            var $target = $( event.currentTarget );
-            $target.closest( '.btn-group' ).find( '[data-bind="label"]' )
-            .text( $target.text() ).end().children( '.dropdown-toggle' ).dropdown( 'toggle' );
-            return false;
- 
-<<<<<<< HEAD
-   return false;
- 
-});
-		</script>
-    
+        <meta charset="UTF-8">
+        <title>Edit Product</title>
+        <?php $this->load->view('partials/meta'); ?>
+        <script>
+            $(document.body).on( 'click', '.dropdown-menu li', function( event ) {
+                var $target = $( event.currentTarget );
+                $target.closest( '.btn-group' ).find( '[data-bind="label"]' )
+                .text( $target.text() ).end().children( '.dropdown-toggle' ).dropdown( 'toggle' );
+                return false;
+            });
+        </script>
         <style type="text/css">
-
-            .jumbotron, .navbar {
-                background-color: white;
-                border: 1px solid e7e7e7;
+            .form-horizontal input {
+                width: 350px;
             }
 
-            .form-horizontal input {
-          	width: 350px;
-          }
-
-          .form-control {
-          	width: 350px;
-          }
-
-            </style>
-</head>
-<body>
-	<div class="container">
-            <?php $this->load->view('partials/nav-admin'); ?>
+            .form-control {
+                width: 350px;
+            }
+         </style>
+    </head>
+    <body>
+	   <div class="container">
+        <?php $this->load->view('partials/nav-admin'); ?>
 
 	<h2>Edit a product</h2>
 	
@@ -58,108 +46,40 @@
 
  		<div class="form-group">
     		<label for="description" class="col-sm-2 control-label">Description: </label>
-    		<div class="col-sm-4">
+    		<div class="col-sm-6">
       			<textarea class="form-control"  rows ="5" name='description' id="prod_text" > <?= $product['description'] ?> </textarea>
    			</div>
   		</div>
 
-			
-    <div class="form-group">
-        <label for="name" class="col-sm-2 control-label">Inventory Count </label>
-        <div class="col-sm-6">
-            <input type="text" class="form-control"  name='inventory' id="prod_name"  value ="<?= $product['inventory_count'] ?>">
-        </div>
-      </div>
-
-      <div class="form-group"> 
-      <div class="col-sm-5 col-sm-offset-2"> 
-        <label for="sell">Select list:</label>
-			  <select class="form-control" id="sell">
-			    <?php foreach($cat_results as $cat_result){?>
-          <option value = "<?= $cat_result['id']?>"><?= $cat_result['name']?></option>
-         <?php }?>
-			  </select>
-			</div><br>
-
-  		<div class="form-group">
-        <div class="col-sm-5 col-sm-offset-2">
-    		<br><label for="exampleInputFile">Images</label>
-   		 	<input type="file" id="exampleInputFile"><br>
-			  <div class="col-sm-2 col-sm-offset-2">
-			  <input type="submit" value="Update">
-		</div>
-		</form>
-=======
-        });
-    </script>
-    <style type="text/css">
-        .jumbotron, .navbar {
-            background-color: white;
-            border: 1px solid e7e7e7;
-        }
->>>>>>> FETCH_HEAD
-
-        .form-horizontal input {
-            width: 350px;
-        }
-
-        .form-control {
-            width: 350px;
-        }
-    </style>
-    </head>
-    <body>
-        <div class="container">
-        <?php $this->load->view('partials/nav-admin'); ?>
-        <h2>Edit a product</h2>
-            <form class="form-horizontal" action="/dashboard/update_product" method="post">
-                <input type="hidden" name="id" value="<?= $product['id'] ?>">
-                <div class="form-group">
-                    <label for="name" class="col-sm-2 control-label">Name: </label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" name='name'  id="prod_name"  value ="<?= $product['name'] ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="name" class="col-sm-2 control-label">Price </label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control"  name='price' id="prod_name"  value ="<?= $product['price'] ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="description" class="col-sm-2 control-label">Description: </label>
-                    <div class="col-sm-10">
-                        <textarea class="form-control"  rows ="5" name='description' id="prod_text" > 
-                            <?= $product['description'] ?> 
-                        </textarea>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="sell">Select list:</label>
-                    <select class="form-control" id="sell">
+        <div class="form-group">
+            <label for="sell" class="col-sm-2 control-label">Select list:</label>
+            <div class="col-sm-6"> 
+                <select class="form-control" id="sell">
                     <?php foreach($cat_results as $cat_result){?>
                         <option value = "<?= $cat_result['id']?>"><?= $cat_result['name']?></option>
-                    <?php }?>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="name" class="col-sm-2 control-label">or add a new category:</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="prod_name" placeholder="New category....">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="name" class="col-sm-2 control-label">Inventory Count </label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control"  name='inventory' id="prod_name"  value ="<?= $product['inventory_count'] ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputFile">Images</label>
-                    <input type="file" id="exampleInputFile">
-                    <input type="submit" value="Update">
-                </div>
-            </form>
-		</div>
-	</body>
+                    <?php } ?>
+                <select>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label for="name" class="col-sm-2 control-label">Inventory Count </label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control"  name='inventory' id="prod_name"  value ="<?= $product['inventory_count'] ?>">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputFile" class="col-sm-2 control-label">Images</label>
+            <div class="col-sm-6">
+                <input type="file" id="exampleInputFile">
+            </div>
+        </div>
+                    
+        <div class="form-group" >
+            <div class="col-sm-offset-2 col-sm-2">
+                <input type="submit" value="Update">
+            </div>
+        </div>
+    </form>
 </html>
